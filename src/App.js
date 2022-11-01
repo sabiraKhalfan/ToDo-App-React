47aca5e89b1b2fc370539a3d443c31d7ca8ff0f0
sabiraKhalfan <109642751+sabiraKhalfan@users.noreply.github.com>
2022-11-01 19:46:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Todo from './components/Todoapp/ToDo'
+function App() {
+  return (
+    <div className="App">
+  <Todo/>
+
+    </div>
+  );
+}
+
+export default App;
